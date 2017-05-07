55a7b73550f18c5651db294009c470a5b7dfc6d1
Julie Ng <me@julie.io>
2017-05-07 08:19:57 +0200

    feat(sass): use sass for stylesheets

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./static/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./static/styles'))
+})
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./static/styles/*.scss', ['sass'])
+})
