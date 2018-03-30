0f95bfb41eee72d081b66f98bd259d3ba5dce034
Julie Ng <me@julie.io>
2018-03-30 13:08:59 +0200

    feat(bootstrap): improve importing, use latest variables

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./static/styles/styles.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./static/styles'))
-})
+    .pipe(
+      sass({includePaths: ['./node_modules/bootstrap/scss']})
+      .on('error', sass.logError))
+    .pipe(gulp.dest('./static/styles'));
+});
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./static/styles/**/*.scss', ['sass'])
-})
+  gulp.watch('./static/styles/**/*.scss', ['sass']);
+});
