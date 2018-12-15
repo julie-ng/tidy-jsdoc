9c75f84c4a1481b24420e1c3b9f5f0671044ef77
Julie Ng <me@julie.io>
2018-12-15 11:25:31 +0100

    feat(nav): add parentheses to functions and style

--- publish.js
+++ publish.js
@@ -339,8 +339,7 @@ function buildMemberNav(items, itemHeading, itemsSeen, linktoFn) {
                         if (method.inherited && conf.showInheritedInNav === false) {
                             return
                         }
-
-                        nav.push(buildNavItem(buildNavType(method.kind, linkto(method.longname, method.name))))
+                        nav.push(buildNavItem(buildNavType(method.kind, linkto(method.longname, method.name + '<span class="method-parens">()</span>'))))
                     })
                 }
 
